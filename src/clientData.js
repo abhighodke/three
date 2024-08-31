a293d5659c0090a0a06dd01b9cf5cc47bbc2950f
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-31 13:34:35 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,35 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "GG", // Header Initials
-    heroTagline: "Gil Gildore Realty", // Hero Tagline
-    agentName: "Gil Gildore",
-    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.", // AboutMe Short Description
-    aboutMeDetailed: `"It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget .Also, I have helped many investors find the perfect property to complete their investment portfolio. 
+    headerInitials: "BD",
+    heroTagline: "Brian Dean Realty",
+    agentName: "Brian Dean",
+    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
+    aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
-Customer Service excellence is our Passion!"`, // AboutMe Detailed Description
-    testimonials: [
-        { quote: "Working with Gil was a game-changer for us. His deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", author: "John and Emily R." },
-        { quote: "Gil Gildore Realty provided exceptional service from start to finish. The team’s professionalism and attention to detail ensured that our property sale was smooth and successful. We couldn’t have asked for a better partner in real estate.", author: "Michael S." },
-        { quote: "The dedication and expertise ofGil Gildore were evident throughout our home-buying journey. His ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", author: "Sarah and David L." },
-        { quote: "Gil and his team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", author: "Jessica and Mark T." },
-        { quote: "If you’re looking for a real estate agent who goes above and beyond, Gil Gildore is the one. His dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", author: "Laura K." }
-    ], // Testimonials
-    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
-    email: "gilgildore@gmail.com", // Email
-    address: "2230 Corporate Circle, Suite 250 Henderson, NV 89074", // Address
-    phone: "+1 (702) 461-8850", // Phone Number
-    footerDescription: "Gil Gildore Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+Customer Service excellence is our Passion!`,
+    testimonials: [{"quote": "Working with Brian Dean was a game-changer for us. His deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Brian Dean provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Brian Dean were evident throughout our home-buying journey. His ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Brian Dean and His team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Brian Dean is the one. His dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    footerTagline: "Your Trusted Partner in Luxury Real Estate",
+    email: "brian@briandean.us",
+    address: "919 6th AVE SE \nDecatur, AL 35601",
+    phone: "256-975-9244",
+    footerDescription: `Brian Dean Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "",
-        twitter: "",
+        facebook: "https://www.facebook.com/brian.dean.75641/",
+        Instagram: "",
         linkedin: "",
-    }, // Social Media Links
+    },
     contactDetails: {
-        email: "gilgildore@gmail.com",
-        phone: "+1 (702) 461-8850",
-    }, // Contact Details
+        email: "brian@briandean.us",
+        phone: "256-975-9244",
+    },
+    website: "",
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/426679430_7630687573608539_7693537339127759889_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=PT1g9GgYRzoQ7kNvgHRzCrm&_nc_ht=scontent-sjc3-1.xx&oh=00_AYD9lIuNOjM49ZgXlMBDB-n2H5kM5rTafHyVgSb-K5iJow&oe=66D7AF4C" // Agent image URL
 };
 
 export default clientData;
