68f034472f7c1da117fba92560c194c064dcff78
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:37:02 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "BD",
-    heroTagline: "Brian Dean Realty",
-    agentName: "Brian Dean",
+    headerInitials: "MR",
+    heroTagline: "Maddie Roberts Realty",
+    agentName: "Maddie Roberts",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Brian Dean was a game-changer for us. His deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Brian Dean provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Brian Dean were evident throughout our home-buying journey. His ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Brian Dean and His team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Brian Dean is the one. His dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Maddie Roberts was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Maddie Roberts provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Maddie Roberts were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Maddie Roberts and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Maddie Roberts is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "brian@briandean.us",
-    address: "919 6th AVE SE \nDecatur, AL 35601",
-    phone: "256-975-9244",
-    footerDescription: `Brian Dean Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "hello@alabamahomegirlgroup.com",
+    address: "3112 Blue Lake Dr Suite 100, Birmingham, AL 35243",
+    phone: "205-616-1304",
+    footerDescription: `Maddie Roberts Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/brian.dean.75641/",
+        facebook: "https://www.facebook.com/not.another.realtoress/",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "brian@briandean.us",
-        phone: "256-975-9244",
+        email: "hello@alabamahomegirlgroup.com",
+        phone: "205-616-1304",
     },
     website: "",
-    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/426679430_7630687573608539_7693537339127759889_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=PT1g9GgYRzoQ7kNvgHRzCrm&_nc_ht=scontent-sjc3-1.xx&oh=00_AYD9lIuNOjM49ZgXlMBDB-n2H5kM5rTafHyVgSb-K5iJow&oe=66D7AF4C" // Agent image URL
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/438255880_3830120927316555_4468344289842912475_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=ADUPfKfogg0Q7kNvgE-autx&_nc_ht=scontent-sjc3-1.xx&oh=00_AYD1Dnav7Eh1cc78AJbS3ISdwFykvWyNom11q0XbZCV6xA&oe=66DBC974" // Agent image URL
 };
 
 export default clientData;
